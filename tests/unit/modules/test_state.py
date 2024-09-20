45a344adc03c2b9439cbf64f3470f0629dd6837b
antonio.velasco <antonio.velasco@bp.com>
2024-09-20 16:52:21 +0200

    upload tests

---
+++ tests/unit/modules/test_state.py
@@ -0,0 +1,14 @@
+from code.modules.state import (
+    read_environ_params,
+    read_url_param_values
+    )
+
+
+def test_read_environ_params():
+    t = read_environ_params()
+    assert "openai_api_key" in t
+
+
+def test_read_url_param_values():
+    t = read_url_param_values()
+    assert "openai_api_key" in t
