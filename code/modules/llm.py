b7f71bb4c730b1790726d4b39024f1c623b0e519
antonio.velasco <antonio.velasco@bp.com>
2024-10-06 20:57:02 +0200

    upload

--- code/modules/llm.py
+++ code/modules/llm.py
@@ -2,9 +2,13 @@ from langchain import OpenAI, PromptTemplate, LLMChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain  # noqa E501
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.chains import create_qa_with_sources_chain, RetrievalQA
 from langchain_core.documents.base import Document
 import itertools as it
 from langchain.callbacks import get_openai_callback
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.vectorstores import Chroma
 
 
 #######################
@@ -108,3 +112,50 @@ def summarize_text(
         if include_costs:
             return joined_summary, cb
         return joined_summary
+
+
+##################
+# Document Query #
+##################
+
+
+def document_queries(pages, query):
+
+    system = f"""
+    You are a helpfull assitant tasked with answering user queries
+    about a provided document.
+    Try to be concise and provide all the information available.
+    When adding the source page, answer with just the number.
+    User query:
+    ###
+    {query}
+    ###
+    """
+
+    # create the open-source embedding function
+    embedding_function = SentenceTransformerEmbeddings(
+        model_name="all-MiniLM-L6-v2")
+    # load it into Chroma
+    db = Chroma.from_documents(pages, embedding_function)
+
+    llm_src = ChatOpenAI(temperature=0, model="gpt-4")
+
+    qa_chain = create_qa_with_sources_chain(llm_src)
+
+    doc_prompt = PromptTemplate(
+        template="Content: {page_content}\n Source page: {page}",
+        input_variables=["page_content", "page"],
+    )
+
+    final_qa_chain = StuffDocumentsChain(
+        llm_chain=qa_chain,
+        document_variable_name='context',
+        document_prompt=doc_prompt,
+    )
+    retrieval_qa = RetrievalQA(
+        retriever=db.as_retriever(),
+        combine_documents_chain=final_qa_chain
+    )
+    response = retrieval_qa.run(system)
+
+    return response
